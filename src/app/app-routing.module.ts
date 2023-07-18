1c3df0e2dfed798898aff66712de3faa93cf10b0
Vinícius Ishikawa <viniyishikawa@gmail.com>
2023-07-17 22:10:18 -0300

    Começando o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Pages
+import { HomeComponent } from './modules/home/pages/home/home.component';
+
+const routes: Routes = [
+
+  //Página padrão será a HomeComponent
+  { path: "", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
