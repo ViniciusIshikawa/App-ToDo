81013140dc551089edf5b1ace2370bef222f5c0d
Vinícius Ishikawa <viniyishikawa@gmail.com>
2023-07-24 22:35:29 -0300

    Adiciona e Deleta itens

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+  public taskList: Array<TaskList> = [];
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false });
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1); //Remove apenas um item, que é o index event.
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+
+    if(confirm) {
+      this.taskList = [];
+    }
+  }
 }
