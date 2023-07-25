d9a6bef40ead21016869cecf660d8a3031bd09f9
Vinícius Ishikawa <viniyishikawa@gmail.com>
2023-07-25 18:10:30 -0300

    Ordenação de itens e validação de input

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 //Interface
 import { TaskList } from '../../model/task-list';
@@ -8,9 +8,13 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
+
+  ngDoCheck(): void {
+    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));//Ordenação
+  }
 
   public setEmitTaskList(event: string) {
     this.taskList.push({ task: event, checked: false });
@@ -25,6 +29,16 @@ export class TodoListComponent {
 
     if(confirm) {
       this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number) {
+    if(!event.length) {
+      const confirm = window.confirm("Task vazia, deseja Deletar?");
+
+      if(confirm) {
+        this.deleteItemTaskList(index);
+      }
     }
   }
 }
